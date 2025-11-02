63f3856550a03ddf85ef3610036060d6507b03b1
Saquib Hazari <saquibhazari1000@gmail.com>
2025-11-02 23:59:43 +0530

    Build navbar and footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,7 +1,89 @@
-import React from "react";
+import { Box, Flex, Heading, Link, Text } from "@chakra-ui/react";
+import { FaDiscord, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { Link as RouterLink } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const linkItems = [
+    { path: "/about", label: "About" },
+    { path: "/contact", label: "Contact" },
+    { path: "/pricing", label: "Pricing" },
+    { path: "/services", label: "Services" },
+  ];
+  return (
+    <Box p={"20"}>
+      <Flex gap={10} justifyContent={"center"} alignItems={"center"}>
+        <div>
+          <Heading
+            size={"4xl"}
+            gradientFrom={"whiteAlpha.300"}
+            gradientTo={"blue.400"}
+          >
+            Sabit Hazari
+          </Heading>
+          <Text
+            color={"gray.600"}
+            fontSize={"md"}
+            lineHeight={"1.6"}
+            width={{ base: "100%", md: "60%" }}
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            molestiae provident harum inventore iste ipsum autem minus quod
+            facilis tempore?
+          </Text>
+        </div>
+        <Flex gap={8} justifyContent={"space-between"} alignItems={"center"}>
+          <Flex gap={4} flexDirection={"column"}>
+            {linkItems.map((item) => (
+              <Link
+                key={item.path}
+                as={RouterLink}
+                to={item.path}
+                fontSize={"18px"}
+                fontWeight={"bold"}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </Flex>
+          <Flex gap={4} flexDirection={"column"}>
+            {linkItems.map((item) => (
+              <Link
+                key={item.path}
+                as={RouterLink}
+                to={item.path}
+                fontSize={"18px"}
+                fontWeight={"bold"}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </Flex>
+          <Flex gap={4} flexDirection={"column"}>
+            {linkItems.map((item) => (
+              <Link
+                key={item.path}
+                as={RouterLink}
+                to={item.path}
+                fontSize={"18px"}
+                fontWeight={"bold"}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </Flex>
+        </Flex>
+      </Flex>
+      <Flex justifyContent={"space-between"} marginTop={7}>
+        <Flex gap={2}>
+          <FaInstagram size={20} cursor={"pointer"} />
+          <FaLinkedin size={20} cursor={"pointer"} />
+          <FaDiscord size={20} cursor={"pointer"} />
+          <FaTwitter size={20} cursor={"pointer"} />
+        </Flex>
+        <p>&copy; 2025 Copy Right Reserved by Sabit Hazari</p>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Footer;
--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,7 +1,53 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Flex, Link } from "@chakra-ui/react";
+import { useLocation, Link as RouterLink } from "react-router-dom";
 
 const Navbar = () => {
-  return <Box>Navbar</Box>;
+  const location = useLocation();
+
+  const navItems = [
+    { path: "/", label: "Home" },
+    { path: "/project", label: "Project" },
+    { path: "/about", label: "About" },
+    { path: "/contact", label: "Contact" },
+  ];
+
+  return (
+    <Box as="nav" bg="black" py={4} px={20}>
+      <Flex
+        bgGradient="linear(to-br, blue.400, purple.500, pink.500)"
+        justifyContent={"space-between"}
+        gap={8}
+        bg="rgba(255, 255, 255, 0.1)"
+        backdropFilter="blur(10px)"
+        border="1px solid rgba(255, 255, 255, 0.2)"
+        borderRadius="2xl"
+        p={5}
+        boxShadow="0 8px 32px rgba(0, 0, 0, 0.1)"
+      >
+        <Link fontSize={24} fontWeight={"bold"}>
+          Sabit Hazari
+        </Link>
+
+        <Flex gap={6}>
+          {navItems.map((items) => (
+            <Link
+              key={items.path}
+              as={RouterLink}
+              to={items.path}
+              borderStyle={"none"}
+              fontWeight={location.pathname === items.path ? "bold" : "normal"}
+              color={location.pathname === items.path ? "white" : "gray.600"}
+            >
+              {items.label}
+            </Link>
+          ))}
+          <Button as={RouterLink} to={"/login"}>
+            Login
+          </Button>
+        </Flex>
+      </Flex>
+    </Box>
+  );
 };
 
 export default Navbar;
