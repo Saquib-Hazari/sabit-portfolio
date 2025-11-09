c4162adf3ad91edd9905be850cef6859fd766e9f
Saquib Hazari <saquibhazari1000@gmail.com>
2025-11-09 11:12:43 +0530

    modified frontend and backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 // In your api service file
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = "https://sabit-portfolio.onrender.com";
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
