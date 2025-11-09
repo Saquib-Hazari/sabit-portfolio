3de4905da2fc60d0c1d393e84618340145cb4a5f
Saquib Hazari <saquibhazari1000@gmail.com>
2025-11-09 12:19:54 +0530

    modified frontend and backend

--- backend/src/controller/userController.ts
+++ backend/src/controller/userController.ts
@@ -107,8 +107,8 @@ class AuthUserController {
 
       res.cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        secure: true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
@@ -137,7 +137,7 @@ class AuthUserController {
       res.clearCookie("token", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
       });
       res.status(200).json({
         success: true,
