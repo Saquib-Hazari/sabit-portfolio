a9ebf100f50027362667917aee85db9082c908fb
Saquib Hazari <saquibhazari1000@gmail.com>
2025-11-09 11:18:53 +0530

    modified frontend and backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ connectedDB();
 
 app.use(
   cors({
-    origin: "*",
+    origin: "https://sabit-portfolio-sigma.vercel.app/",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
