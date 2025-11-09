09bdf844760634882759063640e495822ca6cc16
Saquib Hazari <saquibhazari1000@gmail.com>
2025-11-09 10:28:03 +0530

    modified frontend and backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,7 +11,7 @@ connectedDB();
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "https://sabit-portfolio.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: [
